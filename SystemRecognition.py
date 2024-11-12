caa37e6b07276db10f0ef418c2e9644ae0906832
marcodelama <mbarranca17@gmail.com>
2024-11-12 18:04:12 -0500

    avances unI

--- SystemRecognition.py
+++ SystemRecognition.py
@@ -8,8 +8,29 @@ from PIL import Image, ImageTk
 import imutils
 import math
 
+def Log_Biometric():
+  global pantalla2, conteo, parpadeo, img_info, step, cap, lblVideo
+
+  #Validar videocaptura
+  if cap is not None:
+    ret, frame = cap.read()
+
+    frame = imutils.resize(frame, width=1280)
+
+    #Conversión
+    im = Image.fromarray(frame)
+    img = ImageTk.PhotoImage(image=im)
+
+    #Mostrar video
+    lblVideo.configure(image=img)
+    lblVideo.image = img
+    lblVideo.after(10, Log_Biometric)
+
+  else:
+    cap.release()
+
 def Log():
-  print("Hola Sing in")
+  print("Hola Login")
 
 def Sign():
   global RegName, RegUser, RegPassword, InputNameReg, InputUserReg, InputPasswordReg, cap, lblVideo, pantalla2
@@ -21,12 +42,58 @@ def Sign():
     print("Formulario incompleto")
   else:
     #Validar usuarios
-    UserList = os.listdir(OutFolderPathUser) #Lista de usuarios
+    UserList = os.listdir(PathUserCheck) #Lista de usuarios
+    print(UserList)
+
+    UserName = []
+
+    for list in UserList:
+      User = list
+      User = User.split(".")
+      print(User)
+      UserName.append(User[0])
+
+    if RegUser in UserName:
+      print("Usuario ya registrado")
+
+    else:
+      info.append(RegName)
+      info.append(RegUser)
+      info.append(RegPassword)
+
+      #Exportar información
+      f = open(f"{OutFolderPathUser}/{RegUser}.txt", "w")
+
+      f.write(RegName + ', ' + RegUser + ', ' + RegPassword)
+      f.close()
+
+      InputNameReg.delete(0, END)
+      InputUserReg.delete(0, END)
+      InputPasswordReg.delete(0, END)
+
+      pantalla2 = Toplevel(pantalla)
+      pantalla2.title("LOGIN BIOMÉTRICO")
+      pantalla2.geometry("1280x720")
+
+      # Video
+      lblVideo = Label(pantalla2)
+      lblVideo.place(x=0, y=0)
+
+      cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+      cap.set(3, 1280)
+      cap.set(4, 720)
+      Log_Biometric()
 
 #Rutas
 OutFolderPathUser = 'C:/Users/usuario/Desktop/Marco/RecognitionSystem/Database/Users'
 PathUserCheck = 'C:/Users/usuario/Desktop/Marco/RecognitionSystem/Database/Users/'
 OutlFolderPathFace = 'C:/Users/usuario/Desktop/Marco/RecognitionSystem/Database/Faces'
+
+#Variables
+parpadeo = False
+conteo = 0 
+step = 0
+muestra = 0
 
 #Lista de información
 info = []
@@ -61,12 +128,12 @@ InputPasswordLog.place(x=750, y=500)
 #Botones
 #Registro
 imagenBR = PhotoImage(file='C:/Users/usuario/Desktop/Marco/RecognitionSystem/SetUp/BtLogin.png')
-BtReg = Button(pantalla, text="Registro", image=imagenBR,  height="40", width="200", command=Sign())
+BtReg = Button(pantalla, text="Registro", image=imagenBR,  height="40", width="200", command=Sign)
 BtReg.place(x=300, y=580)
 
 #Inicio
 imagenBI = PhotoImage(file='C:/Users/usuario/Desktop/Marco/RecognitionSystem/SetUp/BtSign.png')
-BtInicio = Button(pantalla, text="Registro", image=imagenBI,  height="40", width="200", command=Log())
+BtInicio = Button(pantalla, text="Registro", image=imagenBI,  height="40", width="200", command=Log)
 BtInicio.place(x=900, y=580)
 
 pantalla.mainloop()
